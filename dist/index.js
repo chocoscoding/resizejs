70487b1405dae8234cea76bbc23b63220a0e67c2
Chocos coding <chocoscoding@gmail.com>
2022-02-24 14:45:36 +0100

    starting ts

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.resize = void 0;
+function resize(domarr) {
+    return 'done';
+}
+exports.resize = resize;
---
+++ index.ts
@@ -0,0 +1,23 @@
+interface mainS{}
+function resize(
+    domarr: [
+        {
+            parent: any,
+            element: HTMLElement,
+            mediaQ: [{
+                    Q: String,
+                    size: String | number
+            }],
+
+
+        }
+    ]
+){
+
+    return 'done'
+
+}
+
+export {
+    resize
+}
