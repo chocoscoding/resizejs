b1b90aec1e96889f375dbe8b50e4f6648e876670
Chocos coding <chocoscoding@gmail.com>
2022-02-25 12:53:05 +0100

    .

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.resize = void 0;
 function resize(domarr) {
     return 'done';
 }
-exports.resize = resize;
+exports.default = {
+    resize
+};
--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ function resize(
 
         }
     ]
-){
+){ 
 
     return 'done'
 
